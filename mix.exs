cbd55b1fa3cd784725114d8a3bbf5268136b25d8
Preston Guillory <pguillory@gmail.com>
2024-02-26 14:06:19 -0800

    Rename app from memcache to memcached

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Memcache.MixProject do
   def project do
     [
       package: package(),
-      app: :memcache,
+      app: :memcached,
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
