38e4f7837a1b9f9c7d937eb747eac7011050af8f
Preston Guillory <pguillory@gmail.com>
2024-04-07 11:01:21 -0700

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Memcache.MixProject do
     [
       package: package(),
       app: :memcached,
-      version: "0.1.2",
+      version: "0.2.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
